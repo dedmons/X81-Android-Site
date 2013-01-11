f35c2de49d5a8df39e44a523864eaf983a2092f1
DJ Edmonson <djedmonson@gmail.com>
2013-01-11 13:48:01 -0500

    Generalized moving side bar to work with 'almost' any page that uses it

--- public/js/site.js
+++ public/js/site.js
@@ -6,19 +6,25 @@ $('.alert-close-btn').click(function(){
     $('#pageNotAvalible').slideUp();
 });
 
+var trigerPoint = -1;
 
 $(window).scroll(function(){
-    var offset = $('body').scrollTop();
+    var pageOffset = $('body').scrollTop();
     var elem = $('.sidebar-nav-affix-top');
+    var elemTop = elem.offset().top;
 
-    if(offset > 320){
+    if(elemTop - pageOffset < 60 && trigerPoint === -1){
+        trigerPoint = pageOffset;
+        console.log(pageOffset+" : "+elemTop);
         var curOff = elem.offset();
         var width = elem.width();
         elem.css('position','fixed');
         elem.animate({top:60,width:width},0);
     }
-    else{
+    else if(pageOffset < trigerPoint){
+        trigerPoint = -1;
         elem.css('position','static');
         elem.css('width','auto');
     }
 });
+
