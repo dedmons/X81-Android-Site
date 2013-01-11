b83e5dad13a9734d046b64519a98f51c3c34677b
DJ Edmonson <djedmonson@gmail.com>
2013-01-11 10:23:51 -0500

    Got site back up to status from last nights losses

--- public/js/site.js
+++ public/js/site.js
@@ -5,3 +5,19 @@ $('.pna').click(function() {
 $('.alert-close-btn').click(function(){
     $('#pageNotAvalible').slideUp();
 });
+
+$(window).scroll(function(){
+    var offset = $('body').scrollTop();
+    var elem = $('.sidebar-nav-affix-top');
+
+    if(offset > 320){
+        var curOff = elem.offset();
+        var width = elem.width();
+        elem.css('position','fixed');
+        elem.animate({top:60,width:width},0);
+    }
+    else{
+        elem.css('position','static');
+        elem.css('width','auto');
+    }
+});
